6a123ac3702918d0fe184de19602f58635b2b2cd
Juan Elosua <juan.elosua@gmail.com>
2016-04-19 12:13:19 +0200

    add react-route initial configuration

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
+// Router: Decides what components to render depending on the URL
+// browserHistory: Means whenever url changes, 
+// Router will interpret everything after the protocol
+// Others: hashHistory and memoryHistory
+import { Router, browserHistory } from 'react-router' 
 import reducers from './reducers';
+import routes from './routes';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <Router history={browserHistory} routes={routes} />
   </Provider>
   , document.querySelector('.container'));
